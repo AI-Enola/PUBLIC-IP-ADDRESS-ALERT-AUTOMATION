f2cf8cd1e75ee7ed37f12ff841cf6dbc95177cca
AI-Enola <75545356+AI-Enola@users.noreply.github.com>
2021-11-23 10:34:19 -0500

    Add files via upload

--- regex.py
+++ regex.py
@@ -1,5 +1,5 @@
 import re
 
 
-def check_ip(ip=str) -> str:
+def check_ip(ip=str) -> object:
     return re.search("((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}", ip) # If match not found return None else it return match object
